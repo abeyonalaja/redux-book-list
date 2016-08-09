111e32551e666f17c3fbcf7d4b13d932f968327a
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-09 09:57:31 +0100

    Add a reducer for books state

---
+++ src/reducers/books.js
@@ -0,0 +1,9 @@
+
+export default function(){
+  return [
+    { title: 'Javascript: The Goood Parts' },
+    { title: 'Harry Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' }
+  ];
+}
